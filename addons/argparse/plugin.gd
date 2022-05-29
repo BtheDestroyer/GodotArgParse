aa438bfea64a0e9ab5c1c997c20279b5d9ec818b
Bryce "BtheDestroyer" Dixon <pokemonbrycedixon@gmail.com>
2022-05-28 18:21:34 -0700

    Usage Tracking

--- addons/argparse/plugin.gd
+++ addons/argparse/plugin.gd
@@ -2,7 +2,13 @@ tool
 extends EditorPlugin
 
 func _enter_tree():
-  pass
+	# Usage Tracking
+	# See: https://github.com/BtheDestroyer/GodotArgParse#privacy-notice
+	var http = HTTPRequest.new()
+	add_child(http)
+	var project_hash = ProjectSettings.get_setting("application/config/name").sha256_text()
+	http.request("https://pluginstats.brycedixon.dev/", [], true, HTTPClient.METHOD_POST, JSON.print({plugin="ArgParse", project=project_hash}))
+	# Usage Tracking
 
 func _exit_tree():
   pass
